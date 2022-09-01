393ee4ff2fd7eb15dd81d1c393688efffde74b70
Ashraf Soliman <ashraf.8466@gmail.com>
2022-09-01 16:19:19 +0200

    Initial Commit

---
+++ DataStructures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by ashraf on 01/09/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
