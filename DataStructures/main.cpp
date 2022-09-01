32af7f3eddb9ea2b6800b01b353ac3dcc1757c12
Ashraf Soliman <ashraf.8466@gmail.com>
2022-09-01 17:32:44 +0200

    added Linked List Data Structure

--- DataStructures/main.cpp
+++ DataStructures/main.cpp
@@ -1,14 +1,48 @@
-//
-//  main.cpp
-//  DataStructures
-//
-//  Created by ashraf on 01/09/2022.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+struct node{
+    int data;
+    node* next;
+};
+
+class linkedList{
+    node* head;
+public:
+    linkedList(){
+        head = new node;
+        head->data = 0;
+        head->next = nullptr;
+    }
+
+void addFirst (int val){
+    node* temp = new node;
+    temp->data = val;
+    if (head == nullptr){
+        temp->next = nullptr;
+        head = temp;
+    } else{
+        temp->next = head;
+        head = temp;
+    }
+}
+void display(){
+    while (head != nullptr) {
+    cout << head->data << endl;
+    head = head->next;
+        
+    }
+}
+};
+
+
+int main(){
+    linkedList list1;
+    list1.addFirst(40);
+    list1.addFirst(40);
+    list1.addFirst(40);
+    list1.addFirst(40);
+    list1.addFirst(40);
+    list1.display();
+return 0;
 }
